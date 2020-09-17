d94e3d75cf47f4d04e9448588eead81d3d6f43f3
Andrej Jokić <aj.jokic@gmail.com>
2020-09-17 09:07:06 +0200

    #LAGK-23 Scope and hoisting

---
+++ scope.js
@@ -0,0 +1,19 @@
+function forLoop() {
+  for (let i=0; i<5; i++) {
+    var test = 'asdf';
+    let test2 = 'fdsa';
+  }
+  console.log(test);
+  //console.log(test2);
+  console.log(test3);
+
+  var test4 = 'asdf';
+}
+
+forLoop();
+
+console.log(test4);
+
+var test3 = 'foo';
+
+forLoop();
